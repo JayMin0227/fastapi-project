3e4dda5877b5cf1bc4437195b42ff31d3e23aa2d
jaymin <ryosakuma0227@gmail.com>
2024-10-30 16:03:02 +0900

    パワー

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
 app = FastAPI()
 
 
@@ -12,4 +14,16 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "t": q}
+    return {"item_id": item_id, "q": q}
+
+# Itemクラスの定義
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item": item}
+
